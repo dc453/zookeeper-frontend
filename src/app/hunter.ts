27996a1cf24e1fdc3167146d6880242825c3671b
David Case <dcase.iai@gmail.com>
2023-02-09 21:47:27 -0500

    add rating spreads to hunter info

--- src/app/hunter.ts
+++ src/app/hunter.ts
@@ -8,3 +8,11 @@ export interface Hunter {
     look_group2?: string[];
     look_group3?: string[];
 }
+
+export interface Ratings {
+    charm: number;
+    cool: number;
+    sharp: number;
+    tough: number;
+    weird: number;
+}
